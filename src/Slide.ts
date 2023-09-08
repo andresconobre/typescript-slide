bfa997f33422ab84d520ea97d5039326d5712302
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-09-08 10:09:39 -0300

    0704 Auto Slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,3 +1,5 @@
+import Timeout from "./Timeout.js";
+
 export default class Slide {
     container: Element;
     slides: Element[];
@@ -5,12 +7,14 @@ export default class Slide {
     time: number;
     index: number;
     slide: Element;
+    timeout: Timeout | null;
     constructor(container: Element, slides: Element[], controls: Element, time: number = 5000) {
         this.container = container
         this.slides = slides
         this.controls = controls
         this.time = time
 
+        this.timeout = null
         this.index = 0
         this.slide = this.slides[this.index]
 
@@ -26,6 +30,12 @@ export default class Slide {
         this.slide = this.slides[this.index]
         this.slides.forEach(el => this.hide(el))
         this.slide.classList.add('active')
+        this.auto(this.time)
+    }
+
+    auto(time: number) {
+        this.timeout?.clear()
+        this.timeout = new Timeout(() => this.next(), time)
     }
 
     prev() {
