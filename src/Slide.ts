51cd19510ec9b5b11a3c1583cbc6c2048ec9cb66
André Pires Nobre <andre.pires.nobre@gmail.com>
2023-09-08 10:56:17 -0300

    0706 Video Element

--- src/Slide.ts
+++ src/Slide.ts
@@ -18,7 +18,7 @@ export default class Slide {
 
         this.timeout = null
         this.pausedTimeout = null
-        this.index = 0
+        this.index = localStorage.getItem('activeSlide') ? Number(localStorage.getItem('activeSlide')) : 0
         this.slide = this.slides[this.index]
         this.paused = false
 
@@ -27,14 +27,33 @@ export default class Slide {
 
     hide(el: Element) {
         el.classList.remove('active')
+        if (el instanceof HTMLVideoElement) {
+            el.currentTime = 0
+            el.pause()
+        }
     }
 
     show(index: number) {
         this.index = index
         this.slide = this.slides[this.index]
+        localStorage.setItem('activeSlide', String(this.index)) //salvando o slide atual no local storage
         this.slides.forEach(el => this.hide(el))
         this.slide.classList.add('active')
-        this.auto(this.time)
+        if (this.slide instanceof HTMLVideoElement) {
+            this.autoVideo(this.slide)
+        } else {
+            this.auto(this.time)
+        }
+    }
+
+    autoVideo(video: HTMLVideoElement) {
+        video.muted = true
+        video.play();
+        let firstPlay = true
+        video.addEventListener('playing', () => {
+            if (firstPlay) this.auto(video.duration * 1000)
+            firstPlay = false
+        })
     }
 
     auto(time: number) {
@@ -58,6 +77,7 @@ export default class Slide {
         this.pausedTimeout = new Timeout(() => {
             this.timeout?.pause()
             this.paused = true
+            if (this.slide instanceof HTMLVideoElement) this.slide.pause()
         }, 300)
     }
 
@@ -66,6 +86,7 @@ export default class Slide {
         if (this.paused) {
             this.paused = false
             this.timeout?.continue()
+            if (this.slide instanceof HTMLVideoElement) this.slide.play()
         }
     }
 
